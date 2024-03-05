19c2738ffb624ce25d5b2539f25a70c280cf8195
AkashKamatCreates <kamatakash13@gmail.com>
2024-03-05 19:16:47 +0530

    commit message

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-function MyApp(){
-  return(
-    <div>
-      <h1> custom app vite</h1>
-    </div>
-  )
-}
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1> custom app vite</h1>
+//     </div>
+//   )
+// }
 
-const anotherElement = (
-  <a href = "www.google.com" target = '_blank'>visit google</a>
-)
+// const anotherElement = (
+//   <a href = "www.google.com" target = '_blank'>visit google</a>
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    anotherElement
+    <App/>
   </React.StrictMode>,
 )

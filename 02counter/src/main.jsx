48ab1e5e0b4e1b8e6bffb01d3e202f6de31c7019
AkashKamatCreates <kamatakash13@gmail.com>
2024-03-05 17:13:05 +0530

    02counter

--- 02counter/src/main.jsx
+++ 02counter/src/main.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-function MyApp(){
-  return(
-    <div>
-      <h1> custom app vite</h1>
-    </div>
-  )
-}
-
-const anotherElement = (
-  <a href = "www.google.com" target = '_blank'>visit google</a>
-)
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    anotherElement
+    <App />
   </React.StrictMode>,
 )

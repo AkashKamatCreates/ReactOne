9c7491d9689f86c6dc3172cad14a09017fd4801b
AkashKamatCreates <kamatakash13@gmail.com>
2024-03-06 14:30:35 +0530

    currency

---
+++ currencyconverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-slate-300 rounded-lg py-3'>Currency App</h1>
+    </>
+  )
+}
+
+export default App
